b16725e2ae320529c136f388644e180e375b00ee
tj003 <tj0032@naver.com>
2026-04-30 12:22:51 +0900

    2026.04.30

---
+++ src/main/java/com/diary/obd_server/ObdServerApplication.java
@@ -0,0 +1,13 @@
+package com.diary.obd_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObdServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObdServerApplication.class, args);
+    }
+
+}
